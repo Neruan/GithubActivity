bc7b12b57cb02bc0e3366de23ab6da22cb6ee200
Neustein <nruan@ucdavis.edu>
2017-03-16 13:42:57 -0700

    Done

--- src/main/java/ecs189/querying/github/GithubQuerier.java
+++ src/main/java/ecs189/querying/github/GithubQuerier.java
@@ -26,29 +26,27 @@ public class GithubQuerier {
         for (int i = 0; i < response.size(); i++) {
             JSONObject event = response.get(i);
             // Get event type
-            String type = event.getString("type" );
-                // Get created_at date, and format it in a more pleasant style
-                String creationDate = event.getString("created_at");
-                SimpleDateFormat inFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
-                SimpleDateFormat outFormat = new SimpleDateFormat("dd MMM, yyyy");
-                Date date = inFormat.parse(creationDate);
-                String formatted = outFormat.format(date);
+            String type = event.getString("type");
+            // Get created_at date, and format it in a more pleasant style
+            String creationDate = event.getString("created_at");
+            SimpleDateFormat inFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
+            SimpleDateFormat outFormat = new SimpleDateFormat("dd MMM, yyyy");
+            Date date = inFormat.parse(creationDate);
+            String formatted = outFormat.format(date);
 
-                //
-
-                // Add type of event as header
-                sb.append("<h3 class=\"type\">");
-                sb.append(i+1 + ". " + type);
-                sb.append("</h3>");
-                // Add formatted date
-                sb.append(" on ");
-                sb.append(formatted);
-                sb.append("<br />");
-                // Add collapsible JSON textbox (don't worry about this for the homework; it's just a nice CSS thing I like)
-                sb.append("<a data-toggle=\"collapse\" href=\"#event-" + i + "\">JSON</a>");
-                sb.append("<div id=event-" + i + " class=\"collapse\" style=\"height: auto;\"> <pre>");
-                sb.append(event.toString());
-                sb.append("</pre> </div>");
+            // Add type of event as header
+            sb.append("<h3 class=\"type\">");
+            sb.append(i + 1 + ". " + type);
+            sb.append("</h3>");
+            // Add formatted date
+            sb.append(" on ");
+            sb.append(formatted);
+            sb.append("<br />");
+            // Add collapsible JSON textbox (don't worry about this for the homework; it's just a nice CSS thing I like)
+            sb.append("<a data-toggle=\"collapse\" href=\"#event-" + i + "\">JSON</a>");
+            sb.append("<div id=event-" + i + " class=\"collapse\" style=\"height: auto;\"> <pre>");
+            sb.append(event.toString());
+            sb.append("</pre> </div>");
         }
         sb.append("</div>");
         return sb.toString();
@@ -56,25 +54,36 @@ public class GithubQuerier {
 
     private static List<JSONObject> getEvents(String user) throws IOException {
         List<JSONObject> eventList = new ArrayList<JSONObject>();
-        String url = BASE_URL + user + "/events?page=1";
-        //System.out.println(url);
-        JSONObject json = Util.queryAPI(new URL(url));
-        //System.out.println(json);
-        JSONArray events = json.getJSONArray("root");
-        for (int j = 1; j < events.length() && eventList.size() < 10; j++) {
-            //eventList.add(events.getJSONObject(j));
-            String num = Integer.toString(j);
-            for (int i = 0; i < events.length(); i++){
-                JSONObject fields = events.getJSONObject(i);
-                String x = fields.getString("type");
-                if (x.equals("PushEvent")){
-                    eventList.add(events.getJSONObject(i));
+        int pageNumber = 1;
+        boolean isNull = false;
+
+        while (eventList.size() != 10 && !isNull){
+            String url = BASE_URL + user + "/events?page=" + pageNumber;
+            //System.out.println(url);
+            JSONObject json = Util.queryAPI(new URL(url));
+            //System.out.println(json);
+            JSONArray events = json.getJSONArray("root");
+        //for (int j = 0; j < events.length(); j++){
+                for (int i = 0; i < events.length() && eventList.size() < 10; i++) {
+                    if(!events.isNull(i)){
+                        JSONObject fields = events.getJSONObject(i);
+                        String fieldType = fields.getString("type");
+                        if(fieldType.equals("PushEvent")){
+                            eventList.add(events.getJSONObject(i));
+                        }
+                    }
                 }
+            pageNumber++;
+            String urlTest = BASE_URL + user + "/events?page=" + pageNumber;
+            //System.out.println(url);
+            JSONObject json2 = Util.queryAPI(new URL(urlTest));
+            //System.out.println(json);
+            JSONArray events2 = json.getJSONArray("root");
+            if(events2.isNull(0)){
+                isNull = true;
             }
-            //url = BASE_URL + user + "/events?page=" + j+1;
-            //json = Util.queryAPI(new URL(url));
-            //events = json.getJSONArray("root");
         }
+        //S}
         return eventList;
     }
 }
