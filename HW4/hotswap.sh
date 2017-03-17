2e55e87c985781428f57db263d6d4bc4e60c0f5a
Neustein <nruan@ucdavis.edu>
2017-03-17 00:04:34 -0700

    README.SUBMISSION

--- HW4/hotswap.sh
+++ HW4/hotswap.sh
@@ -7,7 +7,7 @@ if grep --quiet $1 list.txt
         docker run --network ecs189_default --name web2 -d $1
         docker exec ecs189_proxy_1 /bin/bash /bin/swap2.sh
         echo "Swapping web1 with web2"
-        docker kill web1
+        #docker kill web1
         docker rm -f `docker ps -a | grep web1 | sed -e 's: .*$::'`
     fi
 
@@ -16,7 +16,7 @@ if grep --quiet $1 list.txt
         docker run --network ecs189_default --name web1 -d $1
         docker exec ecs189_proxy_1 /bin/bash /bin/swap1.sh
         echo "Swapping web2 with web1"
-        docker kill web2
+        #docker kill web2
         docker rm -f `docker ps -a | grep web2 | sed -e 's: .*$::'`
     fi
 fi
