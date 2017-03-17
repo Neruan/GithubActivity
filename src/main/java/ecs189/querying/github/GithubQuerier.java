5adafff02d6ab755d682f0dbe67a0b8eb83fa158
Neustein <nruan@ucdavis.edu>
2017-03-17 03:40:51 -0700

    Ready for Submission

--- src/main/java/ecs189/querying/github/GithubQuerier.java
+++ src/main/java/ecs189/querying/github/GithubQuerier.java
@@ -24,29 +24,50 @@ public class GithubQuerier {
         StringBuilder sb = new StringBuilder();
         sb.append("<div>");
         for (int i = 0; i < response.size(); i++) {
-            JSONObject event = response.get(i);
-            // Get event type
-            String type = event.getString("type");
-            // Get created_at date, and format it in a more pleasant style
-            String creationDate = event.getString("created_at");
-            SimpleDateFormat inFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
-            SimpleDateFormat outFormat = new SimpleDateFormat("dd MMM, yyyy");
-            Date date = inFormat.parse(creationDate);
-            String formatted = outFormat.format(date);
+                    JSONObject event = response.get(i);
+                    // Get event type
+                    String type = event.getString("type");
+                    JSONObject payload = event.getJSONObject("payload");
+                    JSONArray commit= payload.getJSONArray("commits");
+                    String commitHash;
+                    String commitMessage;
+                    if(!commit.isNull(0)) {
+                        JSONObject commitFields = commit.getJSONObject(0);
+                        commitHash = commitFields.getString("sha");
+                        commitHash = commitHash.substring(0, Math.min(commitHash.length(), 6));
+                        commitMessage = commitFields.getString("message");
+                    }
+                    else{
+                        commitHash = "No Sha";
+                        commitMessage = "No message";
+                    }
+                    // Get created_at date, and format it in a more pleasant style
+                    String creationDate = event.getString("created_at");
+                    SimpleDateFormat inFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
+                    SimpleDateFormat outFormat = new SimpleDateFormat("dd MMM, yyyy");
+                    Date date = inFormat.parse(creationDate);
+                    String formatted = outFormat.format(date);
 
-            // Add type of event as header
-            sb.append("<h3 class=\"type\">");
-            sb.append(i + 1 + ". " + type);
-            sb.append("</h3>");
-            // Add formatted date
-            sb.append(" on ");
-            sb.append(formatted);
-            sb.append("<br />");
-            // Add collapsible JSON textbox (don't worry about this for the homework; it's just a nice CSS thing I like)
-            sb.append("<a data-toggle=\"collapse\" href=\"#event-" + i + "\">JSON</a>");
-            sb.append("<div id=event-" + i + " class=\"collapse\" style=\"height: auto;\"> <pre>");
-            sb.append(event.toString());
-            sb.append("</pre> </div>");
+                    // Add type of event as header
+                    sb.append("<h3 class=\"type\">");
+                    sb.append(i + 1 + ". " + type);
+                    sb.append("</h3>");
+                    sb.append("<h3 class=\"hash\">");
+                    sb.append( commitHash);
+                    sb.append("</h3>");
+                    sb.append("<h3 class=\"message\">");
+                    sb.append(commitMessage);
+                    sb.append("</h3>");
+
+                    // Add formatted date
+                    sb.append(" on ");
+                    sb.append(formatted);
+                    sb.append("<br />");
+                    // Add collapsible JSON textbox (don't worry about this for the homework; it's just a nice CSS thing I like)
+                    sb.append("<a data-toggle=\"collapse\" href=\"#event-" + i + "\">JSON</a>");
+                    sb.append("<div id=event-" + i + " class=\"collapse\" style=\"height: auto;\"> <pre>");
+                    sb.append(event.toString());
+                    sb.append("</pre> </div>");
         }
         sb.append("</div>");
         return sb.toString();
@@ -68,8 +89,10 @@ public class GithubQuerier {
                     if(!events.isNull(i)){
                         JSONObject fields = events.getJSONObject(i);
                         String fieldType = fields.getString("type");
+
                         if(fieldType.equals("PushEvent")){
                             eventList.add(events.getJSONObject(i));
+                           // String commits = fields.getString("commits");
                         }
                     }
                 }
